dbdf408888030a5c69c70f5be49c459a336b5e50
pinkpink222 <tkdgurtm100@gmail.com>
2025-07-31 17:09:00 +0000

    test

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
 dvwaPageStartup( array( 'authenticated' ) );
 
 $page = dvwaPageNewGrab();
-$page[ 'title' ]   = 'Welcome' . $page[ 'title_separator' ].$page[ 'title' ];
+$page[ 'title' ]   = 'Welcome!' . $page[ 'title_separator' ].$page[ 'title' ];
 $page[ 'page_id' ] = 'home';
 
 $page[ 'body' ] .= "
